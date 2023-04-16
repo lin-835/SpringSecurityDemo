378900ed410917fa5e388b5eaca01126877210c4
lin-835 <78547547+lin-835@users.noreply.github.com>
2023-04-16 16:50:54 +0800

    验证码

--- spring-security-03web/src/main/java/com/baizhi/config/SecurityConfigure.java
+++ spring-security-03web/src/main/java/com/baizhi/config/SecurityConfigure.java
@@ -1,11 +1,15 @@
 package com.baizhi.config;
 
+import com.baizhi.security.filter.KaptchaFilter;
 import com.baizhi.service.MyUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * 自定义 security 相关配置
@@ -16,13 +20,6 @@ public class SecurityConfigure extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyUserDetailService myUserDetailService;
 
-    //数据库实现
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
-//        inMemoryUserDetailsManager.createUser(User.withUsername("root").password("{noop}123").roles("admin").build());
-//        return inMemoryUserDetailsManager;
-//    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -30,24 +27,47 @@ public class SecurityConfigure extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    @Bean//暴露处理
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public KaptchaFilter kaptchaFilter() throws Exception {
+        KaptchaFilter kaptchaFilter = new KaptchaFilter();
+        kaptchaFilter.setFilterProcessesUrl("/doLogin");
+        kaptchaFilter.setUsernameParameter("uname");
+        kaptchaFilter.setPasswordParameter("passwd");
+        kaptchaFilter.setKaptchaParameter("kaptcha");
+        //指定认证管理器
+        kaptchaFilter.setAuthenticationManager(authenticationManagerBean());
+        //指定认证成功处理
+        kaptchaFilter.setAuthenticationSuccessHandler((req, resp, auth) -> {
+            resp.sendRedirect("/index.html");
+        });
+        //指定认证失败处理
+        kaptchaFilter.setAuthenticationFailureHandler((req, resp, ex) -> {
+            resp.sendRedirect("/login.html");
+        });
+        return kaptchaFilter;
+    }
+
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .mvcMatchers("/login.html").permitAll()
+                .mvcMatchers("/vc.jpg").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login.html")//指定自定义登录界面
-                .loginProcessingUrl("/doLogin")
-                .usernameParameter("uname")
-                .passwordParameter("passwd")
-                .defaultSuccessUrl("/index.html", true)
-                .failureUrl("/login.html")//重定向到登录页面
+                .loginPage("/login.html")
                 .and()
-                .logout()//开启退出登录
+                .logout()
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/login.html")
                 .and()
-                .csrf().disable();//csrf 关闭
+                .csrf().disable();
 
+        http.addFilterAt(kaptchaFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
