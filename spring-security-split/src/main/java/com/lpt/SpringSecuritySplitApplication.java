c4b1f3262e8a043289fc3a79372b276736df7967
lin-835 <78547547+lin-835@users.noreply.github.com>
2023-04-16 11:24:08 +0800

    Initial commit

---
+++ spring-security-split/src/main/java/com/lpt/SpringSecuritySplitApplication.java
@@ -0,0 +1,13 @@
+package com.lpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySplitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySplitApplication.class, args);
+    }
+
+}
---
+++ spring-security-split/src/test/java/com/lpt/SpringSecuritySplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySplitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
